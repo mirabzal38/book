02240665ff6c06699af70a583ec60259d8180670
mirabzal38 <mirxabiyevmirabzal148@gmail.com>
2024-04-30 19:42:31 +0500

    html have done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react"
 
 
 
 function Home() {
 
- 
+  const [fromInp, setFRomInp] = useState("")
+  const [toInp, setToInp] = useState("")
+
 
   return (
     <div className='flex p-6 gap-6 bg-slate-600'>
-      
-    <input type="text" placeholder="From ?"/>
-    <input type="text" placeholder="To ?"/>
-    <input type="date" />
-    <input type="date" />
-    <button>book</button>
+
+      <input className="p-3 rounded-xl" type="text" placeholder="From ?" />
+      <input className="p-3 rounded-xl" type="text" placeholder="To ?" />
+      <input type="date" />
+      <input type="date" />
+      <button className="bg-blue-500 p-4 rounded-2xl">book</button>
 
     </div>
   )
